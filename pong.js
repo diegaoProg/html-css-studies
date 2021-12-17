d5fe8b73a076fec84f3da0d3680b8aa95c20bbc8
diegaoProg <diegobarreto.contas@gmail.com>
2021-12-16 22:02:11 -0300

    sharing files

---
+++ pong.js
@@ -0,0 +1,16 @@
+class pong
+{
+    constructor()
+    {
+        this.setup;
+        this.draw();
+    }
+    setup()
+    {
+        createCanvas(100,100);
+    }
+    draw()
+    {
+        background(0);
+    }
+}
